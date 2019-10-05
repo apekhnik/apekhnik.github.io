926d73ca05834be61b6457c5447c65d3e4f9230a
Wcrafting <iam3ji0@gmail.com>
2019-10-05 21:17:12 +0300

    header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 window.onload = () => {
-	
+    let windowsPart = document.querySelectorAll('.window div'),
+        window      = document.querySelector('.window');
+   
+    var layers = document.querySelectorAll('.layer');
+
+    function parallax(event){
+       windowsPart.forEach(function(layer){
+         
+            var speed = layer.getAttribute('data-speed');
+            layer.style.transform = 'translateX('+ (event.clientX*speed)/1000 +'px)';
+            console.log(layer)
+            
+       });
+    }
+
+    document.addEventListener('mousemove', parallax);
 }
